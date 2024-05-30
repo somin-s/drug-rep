3ccb1d6e3b24eab947310dd8c344acae09a268d5
somin-s <sarawoot.somin@gmail.com>
2024-05-31 09:04:17 +1200

    Create dist

---
+++ src/dist/drug-c/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  AppServerModule
+} from "./chunk-ZUL5Z7BR.mjs";
+import "./chunk-XRTTOPCF.mjs";
+import "./chunk-24VIC3GD.mjs";
+export {
+  AppServerModule as default
+};
+//# sourceMappingURL=main.server.mjs.map
